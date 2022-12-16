ff451af916f830b0637852c82694e78be6f6c7b7
Anatoly <anatolyrytik@gmai.com>
2022-12-16 11:33:47 +0100

    move balance to user

--- src/main/java/com/nomoney/paymybuddy/service/TransactionServiceImpl.java
+++ src/main/java/com/nomoney/paymybuddy/service/TransactionServiceImpl.java
@@ -1,9 +1,10 @@
 package com.nomoney.paymybuddy.service;
 
-import com.nomoney.paymybuddy.model.Account;
 import com.nomoney.paymybuddy.model.Transaction;
+import com.nomoney.paymybuddy.model.User;
 import com.nomoney.paymybuddy.repository.AccountRepository;
 import com.nomoney.paymybuddy.repository.TransactionRepository;
+import com.nomoney.paymybuddy.repository.UserRepository;
 import com.nomoney.paymybuddy.util.exception.NotEnoughMoneyException;
 import com.nomoney.paymybuddy.util.exception.NotFoundException;
 import org.springframework.transaction.annotation.Transactional;
@@ -13,29 +14,30 @@ import java.util.List;
 public class TransactionServiceImpl implements TransactionService {
     private final TransactionRepository transactionRepository;
     private final AccountRepository accountRepository;
+    private final UserRepository userRepository;
 
-    public TransactionServiceImpl(TransactionRepository transactionRepository, AccountRepository accountRepository) {
+    public TransactionServiceImpl(TransactionRepository transactionRepository, AccountRepository accountRepository, UserRepository userRepository) {
         this.transactionRepository = transactionRepository;
         this.accountRepository = accountRepository;
+        this.userRepository = userRepository;
     }
 
     @Transactional
-    public Transaction create(Transaction transaction) {
+    public Transaction createInternalTransaction(Transaction transaction) {
         double amount = transaction.getAmount();
 
-        Account userAccount = this.accountRepository.findByEmail(transaction.getUserEmail())
-                .orElseThrow(() -> new NotFoundException("User account not found"));
+        User user = userRepository.findByEmail(transaction.getUserEmail())
+                .orElseThrow(() -> new NotFoundException("User not found"));
 
-        Account recipientAccount = this.accountRepository.findByEmail(transaction.getEmailRecipient())
+        User recipient = userRepository.findByEmail(transaction.getEmailRecipient())
                 .orElseThrow(() -> new NotFoundException("Recipient account not found"));
 
-        if (userAccount.getBalance() >= amount) {
-            userAccount.setBalance(userAccount.getBalance() - amount);
-            recipientAccount.setBalance(recipientAccount.getBalance() + amount);
-            this.accountRepository.save(userAccount);
-            this.accountRepository.save(recipientAccount);
-
-            return this.transactionRepository.save(transaction);
+        if (user.getBalance() >= amount) {
+            user.setBalance(user.getBalance() - amount);
+            recipient.setBalance(recipient.getBalance() + amount);
+            userRepository.save(user);
+            userRepository.save(recipient);
+            return transactionRepository.save(transaction);
         } else throw new NotEnoughMoneyException("Not enough money");
     }
 
