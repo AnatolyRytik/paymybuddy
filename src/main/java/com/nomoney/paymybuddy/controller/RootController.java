f09edd3ac21f2e4aa6fe152b291d3e3363203697
Anatoly <anatolyrytik@gmai.com>
2023-01-13 12:16:43 +0100

    add security

---
+++ src/main/java/com/nomoney/paymybuddy/controller/RootController.java
@@ -0,0 +1,19 @@
+package com.nomoney.paymybuddy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class RootController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+}
