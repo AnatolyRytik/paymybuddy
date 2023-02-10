8b0bc0b26210367b1f5585319eecd094fb9f6197
Anatoly <anatolyrytik@gmai.com>
2023-02-10 13:19:04 +0100

    Update populateDB

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -3,9 +3,9 @@ delete from transactions;
 delete from connections;
 
 insert into users (firstName, lastName, email, password, balance)
-values ('Thomas', 'Toto', 'thomas@gmail.com', '$2a$10$lr1QLlbssfqupUpJSCx.2up09FLJ372DesaZdwyn2QJ8s4suIwtxe', 5382.0),
-       ('Bernard', 'Tata', 'bernard@gmail.com', '$2a$10$qIoAE2PvpjJZYhXKUuR48uXKxhIbVQGGTfinwtz2gsLuAUO3V23ny', 4111.0),
-       ('George', 'Titi', 'george@gmail.com', '$2a$10$tTzSRYrlzgPJEIurP/SY9O4L5E2vniyjsyUT/iIq6OJXITffPcLN2', 2190.0);
+values ('Thomas', 'Toto', 'thomas@gmail.com', '$2a$10$bok3TkG4I7lys7D8Ut50YeIQsoBn1jb.Izq6K8enzbOog1mDz8sze', 5000.0),
+       ('Bernard', 'Tata', 'bernard@gmail.com', '$2a$10$E0yBUZITUSRflwVyUERMnu0SaXLMwJ2yenduyamKhtopp1vU3jtEu', 4000.0),
+       ('George', 'Titi', 'george@gmail.com', '$2a$10$V2Y.abPq9PoxorxewwZdeO7pXoZatPV9lyk11TwoICO/Z.kw4v1cm', 2000.0);
 
 insert into transactions (user_email, amount, email_recipient, description)
 values ('george@gmail.com', 100, 'thomas@gmail.com', 'send to friend'),
@@ -16,6 +16,8 @@ values ('george@gmail.com', 100, 'FR89328902', 'top up balance'),
        ('bernard@gmail.com', 200, 'FR8392187', 'top up balance');
 
 insert into connections (user_id, friend_id)
-values (1000,1001),
-       (1000,1002),
-       (1001,1002);
+values (1002,1001),
+       (1002,1000),
+       (1001,1000),
+	   (1000,1001),
+       (1000,1002);
